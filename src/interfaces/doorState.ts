d8297e20eeb2cc7c30332b60d1ded05ee6b8d88c
Kristoffer Ellingsen <k.ellingsen@outlook.com>
2022-12-08 13:01:25 +0100

    Clean up

--- src/interfaces/doorState.ts
+++ src/interfaces/doorState.ts
@@ -1,11 +1,11 @@
-import { Nullable } from "homebridge"
+import { Nullable } from 'homebridge';
 
 export interface DoorState {
-	Label: string
-	PanelId: string
-	Serial: string
-	Status: string
-	SoundLevel: number
-	AutoLockEnabled: boolean
-	Languages: Nullable<string>
+	Label: string;
+	PanelId: string;
+	Serial: string;
+	Status: string;
+	SoundLevel: number;
+	AutoLockEnabled: boolean;
+	Languages: Nullable<string>;
 }
